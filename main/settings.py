11d440d52de213828be6e769ef118af4616361ce
mrStorrs <cj.storrs@gmail.com>
2018-01-25 20:21:50 -0800

    again

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+u@b1vn_$9l!s$%i_9!qni)_$)_y+x1bp647pxt%7e^22sjsur'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.220.125.89']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,4 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
